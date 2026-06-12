7b8d1d4dee94dedfb0f5793b9a182b7ff71d47d9
Hole 30601 <121752219+Hole30601@users.noreply.github.com>
2026-06-12 20:15:09 +0700

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = { 
+apiKey: "AIzaSyDshOvDwAkUjVJ_lDOG6CTAtMNc0lIxvyI", authDomain: "hoangdeptrai-d1aaa.firebaseapp.com", 
+databaseURL: "https://hoangdeptrai-d1aaa-default-rtdb.asia-southeast1.firebasedatabase.app", 
+projectId: "hoangdeptrai-d1aaa", 
+storageBucket: "hoangdeptrai-d1aaa.firebasestorage.app", 
+messagingSenderId: "764297369562", 
+appId: "1:764297369562:web:488a9b6ff3b30c65f7d2e3", 
+measurementId: "G-8SZGNWE1MJ" 
+};
